7342f056381fe6f85c269a0e650b3559bc129841
!"Nekros <122643715+Nekros-dsc@users.noreply.github.com>
2023-05-03 22:32:44 +0200

    Update index.js

--- index.js
+++ index.js
@@ -5,32 +5,14 @@ const client = new Client({
     checkUpdate: false,
 });
 
-// By https://github.com/Nekros-dsc
-
 client.on('ready', async () => {
     console.log(`Logged in as ${client.user.tag}!`)
 
     const channel = await client.channels.fetch(config.client.bumpChannel)
-    
-    async function bump() {
+          setInterval(async function () {
         await channel.sendSlash('302050872383242240', 'bump')
         console.count('Bumped!')
-    }
-
-    // By https://github.com/Nekros-dsc
-
-    function loop() {
-        // Send bump message every 2 hours, to prevent detection.
-        const delay = 2 * 60 * 60 * 1000 // 2 heures en millisecondes
-        setTimeout(function () {
-            bump()
-            loop()
-        }, delay)
-    }
-    
-    bump()
-    loop()
+        }, 2 * 60 * 60 * 1000)
 })
 
 client.login(config.client.token);
-// By https://github.com/Nekros-dsc
